ea94444470bcb0106eefc9216737b628ccc47c34
zhao-huo-long <lijiuyi1995@outlook.com>
2023-03-06 09:25:22 +0000

    feat: init

---
+++ rollup.config.mjs
@@ -0,0 +1,26 @@
+import typescript from '@rollup/plugin-typescript';
+console.log(process.env.module_type)
+
+const output = {
+  dir: './dist',
+  format: 'cjs',
+}
+
+const tsConfig = {
+  outDir: './dist'
+}
+
+if(process.env.module_type === 'esm'){
+  output.format = 'esm'
+  output.dir = './esm'
+  tsConfig.outDir = './esm'
+}
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    preserveModules: true,
+    ...output
+  },
+  plugins: [typescript(tsConfig)]
+};
