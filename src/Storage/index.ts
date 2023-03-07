d15a8f03c668dde7af08746d18eeb7348576345f
zhao-huo-long <lijiuyi1995@outlook.com>
2023-03-07 21:55:13 +0800

    feat(Storage): add comment & jsdoc

--- src/Storage/index.ts
+++ src/Storage/index.ts
@@ -1,14 +1,39 @@
 import { isBrowser } from "../helper";
+
 /**
- * StorageWithType
+ * @description StorageType 存储类型
  */
 export type StorageType = "localStorage" | "sessionStorage";
 
-export class StorageWithType<T extends LiteralObject = EmptyObject>
-  implements Omit<Storage, "getItem">
-{
+/**
+ * StorageWithType
+ *
+ * @example
+ * ```typescript
+ * interface KeyValType {
+ *  id: number
+ *  username: string
+ * }
+ * const storage = new StorageWithType<KeyValType>('sessionStorage')
+ * storage.setItem('id', 2)
+ * // if you write storage.setItem('id', '2') typescript will throw type error
+ * storage.getItem('id')
+ * // if you write storage.getItem('ids') typescript will throw type error
+ * ```
+ */
+export class StorageWithType<T extends LiteralObject = EmptyObject> {
+  /**
+   * storage
+   * @default `window.localStorage`
+   */
   protected storage = window.localStorage;
+  /**
+   * storage type `localStorage` or `sessionStorage`
+   */
   protected storageType: StorageType = "localStorage";
+  /**
+   * @param type {StorageType}
+   */
   constructor(type: StorageType) {
     if (!isBrowser || !window?.localStorage || !window?.sessionStorage) {
       throw new Error(
@@ -22,12 +47,18 @@ export class StorageWithType<T extends LiteralObject = EmptyObject>
       this.storage = window.sessionStorage;
     }
   }
+  /**
+   * @description 设置存储的 key, value
+   */
   setItem<Key extends keyof T>(key: Key, val: T[Key]) {
     if (typeof key === "string") {
       this.storage.setItem(key, JSON.stringify(val));
     }
     throw new Error("key must be string");
   }
+  /**
+   * @description 通过 key 获取存储的 value
+   */
   getItem<Key extends keyof T>(key: Key): AddNullVal<T>[Key] {
     if (typeof key === "string") {
       const val = this.storage.getItem(key);
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,10 +1,12 @@
 /**
  * EmptyObject
+ * @description 空对象类型
  */
 type EmptyObject = Record<string, never>;
 
 /**
- * EmptyObject
+ * AddNullVal
+ * @deprecated 扩展类型 `value｜ null`
  */
 type AddNullVal<T extends LiteralObject> = {
   [i in keyof T]: T[i] | null;
@@ -12,5 +14,6 @@ type AddNullVal<T extends LiteralObject> = {
 
 /**
  * LiteralObject
+ * @description plain对象类型
  */
 type LiteralObject<T = unknown> = Record<string, T>;
